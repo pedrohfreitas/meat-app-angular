57c7c83db1e460617e42d69e6cb2649a77b27b0f
Pedro Freitas <pedrofreitas@outlook.com>
2018-12-22 20:12:23 -0200

    tratamento de erros

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,7 +1,9 @@
+import { ErrorHandler } from './../app.error-handler';
 import { MEAT_API } from './../app.api';
 import {Injectable} from '@angular/core'
 import {Http} from '@angular/http'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 import { Restaurant } from "./restaurant/restaurant.model"
 import { Observable } from 'rxjs/Observable';
@@ -12,8 +14,12 @@ export class RestaurantsService {
     constructor(private http: Http) {
     }
 
+    //Método chamado apenas quando alguém se escrever (subscribe)
     restaurants(): Observable<Restaurant[]> {
         //O mapeamento é preciso pois o retorno Observable<response> retorna muita informação, por exemplo: O status code
-        return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json());
+        //Catch: sempre que um erro acontece, o Observable é fechado - é possível criar um outro observable retornando um erro ou outro tratamento
+        return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
     }
 }
