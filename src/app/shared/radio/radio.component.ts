6445fc76bb354eadba8b5ab3671d32f4d629a449
Pedro Freitas <pedrofreitas@outlook.com>
2018-12-29 03:01:27 -0200

    Implementação de da interface ControlValueAccessor

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,24 +1,63 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
-import {RadioOption } from './radio-option.model';
+import { RadioOption } from './radio-option.model';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),//Disponível externamente
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: RadioOption[];
 
   value: any;
+
+  onChange: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  setValue(value: any){
+  setValue(value: any) {
     this.value = value;
+    this.onChange(this.value);
   }
+
+  /**
+    * Utilizada quando se deseja passar um valor para o componente
+    */
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+  /**
+   * - Função chamada sempre o que o valor interno do componente mudar
+   * Set the function to be called when the control receives a change event.
+   */
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  /**
+   * Registra se o usuário entrou no componente
+   * Set the function to be called when the control receives a touch event.
+   */
+  registerOnTouched(fn: any): void {
+
+  }
+  /**
+   * This function is called when the control status changes to or from "DISABLED".
+   * Depending on the value, it will enable or disable the appropriate DOM element.
+   *
+   * @param isDisabled
+   */
+  setDisabledState?(isDisabled: boolean): void;
 
 }
