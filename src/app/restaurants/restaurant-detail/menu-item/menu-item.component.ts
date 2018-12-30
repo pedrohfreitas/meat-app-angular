dfa46ecc1f520bb9de98be46e63ccbd76a7dc285
Pedro Freitas <pedrofreitas@outlook.com>
2018-12-30 17:51:55 -0200

    inclusão de animações

--- src/app/restaurants/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurants/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { MenuItem } from './menu-item.model';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations:[
+    trigger('menuItemAppeared', [
+      state('ready', style({ opacity: 1})),
+      transition('void => ready', [ //void é um momento onde o compoenente não existe ainda na arvore de componentes
+          style({opacity: 0, transform: 'translateY(-20px)'}),//o translate com o -30px ele começa em -30px e vai até zero, o mesmo acontece com o -10px
+          animate('300ms 0s ease-in')//entra acelerando a aniamção e termina desacelerando
+      ])
+    ])
+
+  ]
 })
 export class MenuItemComponent implements OnInit {
+
+  menuItemState = "ready";
 
   @Input() menuItem: MenuItem;
   @Output() add = new EventEmitter();
--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Restaurant } from './restaurant.model';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('restaurantAppeared', [
+      state('ready', style({ opacity: 1})),
+      transition('void => ready', [ //void é um momento onde o compoenente não existe ainda na arvore de componentes
+          style({opacity: 0, transform: 'translate(-30px,-10px)'}),//o translate com o -30px ele começa em -30px e vai até zero, o mesmo acontece com o -10px
+          animate('300ms 0s ease-in-out')//entra acelerando a aniamção e termina desacelerando
+      ])
+    ])
+  ]
+
 })
 export class RestaurantComponent implements OnInit {
+
+  restaurantState = 'ready';
 
   //Isso é importante para um component parent poder enviar informações para este component
   //Recebe dados
